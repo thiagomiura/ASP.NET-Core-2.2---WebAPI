d4c54ee8ec13e79ab6811745ea54574f39d237db
Thiago Tamio Miura <thiagotm@jmsegs.com>
2019-05-02 17:03:20 -0300

    Adição do index.html e site.js Tradução de algumas frases

--- TodoItemWebAPI/Controllers/TodoController.cs
+++ TodoItemWebAPI/Controllers/TodoController.cs
@@ -21,8 +21,7 @@ namespace TodoApi.Controllers
             {
                 // Create a new TodoItem if collection is empty,
                 // which means you can't delete all TodoItems.
-                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
-                _context.TodoItems.Add(new TodoItem { Name = "Itemteste" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });                
                 _context.SaveChanges();
             }
         }
--- TodoItemWebAPI/Startup.cs
+++ TodoItemWebAPI/Startup.cs
@@ -45,6 +45,8 @@ namespace TodoItemWebAPI
                 app.UseHsts();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
